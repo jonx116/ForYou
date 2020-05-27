db2cd60e4323ddee8748b10299b5ce5dcf15c1b5
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 02:37:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let usuario = getElementById('useri');
 let contraseña = getElementById('passi');
 let btm = getElementById('btm');
-let add = getElementByClass('add')
+let add = getElementById('add');
 btm.onclick = submit(){
-  if( usuario == '1Mes' && contraseña == 'iloveyou'){
+  if( usuario.value == '1Mes' && contraseña.value == 'iloveyou'){
     add.innerHTML = <h3> https://jonx116.github.io/ForYou/1Mes/index.html </h3>
   }
 }
