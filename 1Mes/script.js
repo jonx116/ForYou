ae107c1c5714447e454209f41fcebfbafd38f08c
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 18:59:39 -0300

    Update script.js

--- 1Mes/script.js
+++ 1Mes/script.js
@@ -8,6 +8,7 @@ document.getElementById("bt").onclick = function show(){
   count += 1;
   if(count == 1){
     document.getElementById("1").style.visibility = "visible";
+    document.getElementById("bt").value = "again";
   }
 }
 
