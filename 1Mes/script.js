98ac5879bbca20db372422cb3367c89fb09fad3d
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 20:11:30 -0300

    Update script.js

--- 1Mes/script.js
+++ 1Mes/script.js
@@ -9,6 +9,8 @@ document.getElementById("bt").onclick = function show(){
   if(count == 1){
     document.getElementById("1").style.visibility = "visible";
     document.getElementById("bt").value = "again";
+  }else if(count == 2){
+      document.getElementById("1").src = "https://qtxasset.com/fiercebiotech/1553681843/connor-wells-534089-unsplash.jpg/connor-wells-534089-unsplash.jpg?XObjNuiIeq00Lv1yNJ92KYHkC1mDagZm";
   }
 }
 
