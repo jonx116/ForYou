1c109125a38519401e14e572eb2fed8b728eaf92
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 15:26:16 -0300

    Update script.js

--- 1Mes/script.js
+++ 1Mes/script.js
@@ -1,11 +1,12 @@
-document.getElementById("h1").style.visible = "visible";
-document.getElementById("bt").style.visible = "visible";
-document.getElementById("h2").style.visible = "hidden";
-document.getElementById("1").style.visible = "hidden";
-document.getElementById("2").style.visible = "hidden";
-document.getElementById("3").style.visible = "hidden";
-document.getElementById("4").style.visible = "hidden";
-document.getElementById("5").style.visible = "hidden";
+document.getElementById("h1").style.visibility = "visible";
+document.getElementById("bt").style.visibility = "visible";
+document.getElementById("h2").style.visibility = "hidden";
+document.getElementById("1").style.visibility = "hidden";
+document.getElementById("2").style.visibility = "hidden";
+document.getElementById("3").style.visibility = "hidden";
+document.getElementById("4").style.visibility = "hidden";
+document.getElementById("5").style.visibility = "hidden";
+
 
 var n = 0;
 
