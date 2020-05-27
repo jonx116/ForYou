e3842ba2d66428a87f8c988c62a6ff43e1597743
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 02:24:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let usuario = getElementById('useri');
+let contraseña = getElementById('passi');
+let btm = getElementById('btm');
+let add = getElementByClass('add')
+btm.onclick = submit(){
+  if( usuario == '1Mes' && contraseña == 'iloveyou'){
+    add.innerHTML = <button type="button" name="go" id="btm" href='./1Mes/index.html'></button>
+  }
+}
