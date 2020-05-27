5a810105f52875132782d14738d54fef46dc2412
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 04:29:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 let usuario    =    document.getElementById('useri').value;
 let contraseña =    document.getElementById('passi').value;
-let btm        =    document.getElementById('btm');
 let add        =    document.getElementById('add');
 
 add.style.visibility = "hidden";
 
-btm.onclick = submit();
+document.getElementById('btm').onclick = function changeContent() {
   
-function submit(){
   if( usuario  == "1Mes" && contraseña == "iloveyou"){
     add.style.visibility = "visible";
     console.log("query OK");
