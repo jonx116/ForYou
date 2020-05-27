e853d4df8f91470a9ebda0d092f0e1267769bcd9
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 18:24:28 -0300

    Update script.js

--- 1Mes/script.js
+++ 1Mes/script.js
@@ -3,7 +3,7 @@ document.getElementById("bt").style.visibility = "visible";
 document.getElementById("h2").style.visibility = "hidden";
 
 var count = 0;
-document.getElementById("bt").onclick = funcion show(){
+document.getElementById("bt").onclick = function show(){
   count += 1;
   if(count == 1){
     document.getElementById("1").style.visibility = "visible";
