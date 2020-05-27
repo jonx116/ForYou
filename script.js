19113fbe5268b16cc81a0e3d10cbf3efc4a8a691
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 04:37:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ let usuario    =    document.getElementById('useri').value;
 let contraseña =    document.getElementById('passi').value;
 let add        =    document.getElementById('add');
 
-add.style.visibility = "hidden";
-
 document.getElementById('btm').onclick = function changeContent() {
   
   if( usuario  == "1Mes" && contraseña == "iloveyou"){
