94b3718bfc60c8ec02fc9ce8ddb55e11f6388fea
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 02:39:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let usuario = getElementById('useri');
 let contraseña = getElementById('passi');
 let btm = getElementById('btm');
-let add = getElementById('add');
+let add = getElementByClass('add');
 btm.onclick = submit(){
-  if( usuario.value == '1Mes' && contraseña.value == 'iloveyou'){
+  if( usuario == '1Mes' && contraseña == 'iloveyou'){
     add.innerHTML = <h3> https://jonx116.github.io/ForYou/1Mes/index.html </h3>
   }
 }
