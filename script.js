6676886311bfaa3e1e5338cb3ac5900296d97648
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 05:05:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById('btm').onclick = function changeContent() {
   if( usuario.value  == "mes" && contraseña.value == "iloveyou"){
     add.style.visibility = "visible";
     console.log("query OK");
-  }else if( usuario.value  != "mes" && contraseña.value != "iloveyou"){
+  }else if( usuario.value  != "mes" || contraseña.value != "iloveyou"){
     console.log("query ERROR");
   }else{
     console.log("FALTAL ERROR");
