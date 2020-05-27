ae7013c4fed00fbfa21051d0b57f259d0339e6bb
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 07:50:15 -0300

    Add files via upload

---
+++ 1Mes/script.js
@@ -0,0 +1,30 @@
+document.getElementById("h1").style.visible = "visible";
+document.getElementById("bt").style.visible = "visible";
+document.getElementById("h2").style.visible = "hidden";
+document.getElementById("1").style.visible = "hidden";
+document.getElementById("2").style.visible = "hidden";
+document.getElementById("3").style.visible = "hidden";
+document.getElementById("4").style.visible = "hidden";
+document.getElementById("5").style.visible = "hidden";
+
+let second = 0;
+
+
+
+function time(){
+  let X = 0;
+  while(X == 0){
+  second += 1;
+  sleep(1000);
+  console.log(second);
+}
+
+
+  function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+}
