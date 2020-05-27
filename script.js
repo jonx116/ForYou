91ad009bb036142f8df4b50126dcff7e7e63d3b1
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 03:27:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let usuario = getElementById('useri');
-let contraseña = getElementById('passi');
-let btm = getElementById('btm');
-let add = getElementById('add');
+let usuario    =    document.getElementById('useri');
+let contraseña =    document.getElementById('passi');
+let btm        =    document.getElementById('btm');
+let add        =    document.getElementById('add');
 
 add.visibility = "hidden";
 
