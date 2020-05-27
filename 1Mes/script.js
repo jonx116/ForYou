7c2250fda737b281f3a12c984ea288822f31aaac
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-27 18:20:32 -0300

    Update script.js

--- 1Mes/script.js
+++ 1Mes/script.js
@@ -1,26 +1,13 @@
 document.getElementById("h1").style.visibility = "visible";
 document.getElementById("bt").style.visibility = "visible";
 document.getElementById("h2").style.visibility = "hidden";
-document.getElementById("1").style.visibility = "hidden";
-document.getElementById("2").style.visibility = "hidden";
-document.getElementById("3").style.visibility = "hidden";
-document.getElementById("4").style.visibility = "hidden";
-document.getElementById("5").style.visibility = "hidden";
 
-
-var n = 0;
-
- document.getElementById("bt").onclick = x();
-  
-  function x(){window.setInterval(function(){
-  console.log(n);
-  n++;
-},1000);
-
-document.getElementById("bt").onclick = function show(){
- n=0;
- while(n < 10 ){
-  document.getElementById("h1").style.visibility = "hidden";
+var count = 0;
+document.getElementById("bt").onclick = funcion show(){
+  count += 1;
+  if(count == 1){
+    document.getElementById("1").style.visibility = "visible";
+  }
 }
-}
-              }
+
+
