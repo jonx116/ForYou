73dc57f250f15680443bedba9d37f00dd06fa348
jonx116 <65995248+jonx116@users.noreply.github.com>
2020-05-28 02:14:22 -0300

    Update script.js

--- 1Mes/script.js
+++ 1Mes/script.js
@@ -2,7 +2,8 @@ document.getElementById("h1").style.visibility = "visible";
 document.getElementById("bt").style.visibility = "visible";
 document.getElementById("h2").style.visibility = "hidden";
 document.getElementById("1").style.visibility  = "hidden";
-      
+document.getElementById("bdy").style = "background-color: #A24CB8;"; 
+
 var count = 0;
 document.getElementById("bt").onclick = function show(){
   count += 1;
